39d48f57bef2c90fcc66ccf72ab5807b144686ad
yonnyZer0 <janzisko@gmail.com>
2018-08-11 16:19:00 +0200

    install x11vnc and run vnc server at the start of the docker

--- node-chrome-xvfb-x11vnc/start_xvfb_and_run_cmd.sh
+++ node-chrome-xvfb-x11vnc/start_xvfb_and_run_cmd.sh
@@ -4,8 +4,11 @@ echo "Starting X virtual framebuffer using: Xvfb $DISPLAY -ac -screen 0 $XVFB_WH
 
 Xvfb $DISPLAY -ac -screen 0 $XVFB_WHD -nolisten tcp &
 
-echo "starting persistent x11vnc server listening on port 5900"
-x11vnc -display :99 -forever -rfbport 5900 &
+echo "Starting persistent x11vnc server listening on port 5900."
+echo "FYI: Run container with parameter e.g. \"-p 5900:5900\" to map container port to local port."
+x11vnc -display :99 -forever -rfbport 5900 -q &
+
+echo "Now you can access it access it by cmd: \"xtigthvncviewer localhost:5900\" ."
 
 # Execute CMD (original CMD of this Dockerfile gets overriden in Actor build)
 echo "Executing main command"
